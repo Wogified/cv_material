cca7a40b9afdd8fb326ad4757ee09f6cf839a50f
Kevin C <Wogified@gmail.com>
2019-11-10 23:15:11 -0500

    implemented nested lists

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import { withStyles } from "@material-ui/core/styles";
+
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Grid from "@material-ui/core/Grid";
@@ -13,11 +12,11 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Collapse from "@material-ui/core/Collapse";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
-import StarBorder from "@material-ui/icons/StarBorder";
+import AccountBalanceRoundedIcon from "@material-ui/icons/AccountBalanceRounded";
 // Resource imports
 import { school } from "../../resources/resume.json";
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   toot: {
     width: "100%",
     backgroundColor: theme.palette.background.paper,
@@ -34,113 +33,61 @@ const useStyles = makeStyles(theme => ({
     flexDirection: "column",
     justify: "space-between"
   }
-}));
+});
 
-const myStyles = makeStyles(theme => ({
-  boot: {
-    flexGrow: 1,
-    padding: theme.spacing(1)
-  },
-  place: {
-    textAlign: "left",
-    fontWeight: "bold",
-    textDecoration: "underline"
-  },
-  date: {
-    textAlign: "right"
-  },
-  deg: {
-    textAlign: "left"
-  },
-  gpa: {
-    textAlign: "right"
+class Education extends React.Component {
+  componentDidMount() {
+    console.log(this.state.expand);
   }
-}));
-
-const renderSections = props => {
-  const classes = myStyles();
-  const handleClick = index => {
-    this.setState({ [index]: true });
+  state = {
+    expand: false,
+    school: school
   };
-  return props.school.degrees.map((item, index) => {
+  // classes = useStyles();
+
+  handleClick = () => {
+    console.log(this.state.expand);
+    this.setState({ expand: !this.state.expand });
+  };
+  handleClick2 = e => {
+    console.log(this.state);
+    this.setState({ [e]: !this.state[e] });
+  };
+
+  renderSections() {
+    const { classes } = this.props;
+    return this.state.school.degrees.map((item, index) => {
+      return (
+        <div>
+          <ListItem button onClick={this.handleClick2.bind(this, index)}>
+            <ListItemIcon>
+              <AccountBalanceRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary={item.place} />
+            {this.state[index] ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={this.state[index]} timeout="auto" unmountOnExit>
+            poop
+          </Collapse>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
     return (
-      <div className={classes.boot}>
-        <List className={classes.toot}></List>
-        {/* <Grid container spacing={0}>
-          <Grid item xs={9} className={classes.place}>
-            {item.place}
-          </Grid>
-          <Grid item xs={3} className={classes.date}>
-            {item.finish_date}
-          </Grid>
-          <Grid item xs={8} className={classes.deg}>
-            {item.degree}
-          </Grid>
-          <Grid item xs={4} className={classes.gpa}>
-            GPA: {item.gpa}
-          </Grid>
-        </Grid> */}
-      </div>
+      <List className={classes.toot}>
+        <ListItem button onClick={this.handleClick}>
+          <ListItemText primary={school.header} />
+          {this.state.expand ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
+        <Collapse in={this.state.expand} timeout="auto" unmountOnExit>
+          {this.renderSections()}
+        </Collapse>
+      </List>
     );
-  });
-};
-
-const Education = props => {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-  const handleClick = () => {
-    setOpen(!open);
-  };
-  const handleClick2 = index => {
-    this.useState({ [index]: true });
-  };
-
-  return (
-    <List className={classes.toot}>
-      <ListItem button onClick={handleClick}>
-        <ListItemText primary={school.header} />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={!open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          {school.degrees.map((item, index) => (
-            <div key={index}>
-              <ListItem button={true} onClick={handleClick2(index)}>
-                <ListItemText
-                  primary={item.place}
-                  secondary={
-                    <p>
-                      <b>Author: </b>
-                      {item.deg}
-                    </p>
-                  }
-                />
-                {props[index] ? <ExpandLess /> : <ExpandMore />}
-              </ListItem>
-              <Collapse in={props[index]} timeout="auto" unmountOnExit={true}>
-                <p>POOP</p>
-              </Collapse>
-            </div>
-          ))}
-        </List>
-      </Collapse>
-    </List>
-  );
-};
-
-export default Education;
-{
-  /* <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          className={classes.summary}
-        >
-          <Typography className={classes.heading}>{school.header}</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails className={classes.details}>
-          <Typography>{</Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel> */
+  }
 }
+
+export default withStyles(styles, { withTheme: true })(Education);
