f44cba34a83104f818d238c06ee90c1d0b088dbb
Kevin C <Wogified@gmail.com>
2019-11-11 22:58:13 -0500

    Updates

---
+++ precache-manifest.6a70c1ee8a575ae324bd3a6b336634cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b403e0e479f6242c90781dc0ed48b640",
+    "url": "/cv_material/index.html"
+  },
+  {
+    "revision": "5f4c39cab4ed9f11ec7c",
+    "url": "/cv_material/static/css/main.0a3f50d4.chunk.css"
+  },
+  {
+    "revision": "cd632f521cbb77cdc9ac",
+    "url": "/cv_material/static/js/2.83f6d1f4.chunk.js"
+  },
+  {
+    "revision": "5f4c39cab4ed9f11ec7c",
+    "url": "/cv_material/static/js/main.87d88fbc.chunk.js"
+  },
+  {
+    "revision": "2b095dabce1d96b54089",
+    "url": "/cv_material/static/js/runtime-main.49dba876.js"
+  }
+]);
